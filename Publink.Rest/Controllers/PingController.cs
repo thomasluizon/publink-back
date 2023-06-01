cd255ad263aca4eadbb308506036666652b5cb65
thomasluizon <thomaslrgregorio@gmail.com>
2023-05-31 22:54:32 -0300

    Add project files.

---
+++ Publink.Rest/Controllers/PingController.cs
@@ -0,0 +1,24 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace Publink.Rest.Controllers
+{
+	[ApiController]
+	[Route("[controller]")]
+	public class PingController : ControllerBase
+	{
+		private readonly ILogger<PingController> _logger;
+
+		public PingController(ILogger<PingController> logger)
+		{
+			_logger = logger;
+		}
+
+		[HttpGet]
+		public IActionResult Ping()
+		{
+			_logger.LogDebug("Calling ping controller");
+
+			return Content("ok");
+		}
+	}
+}
